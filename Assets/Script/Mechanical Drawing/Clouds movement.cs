2b7ce181f6539d7529e65a5982cc55da7027d75d
LAPTOP-BITE348R\Anthony <pattisoa@sheridancollege.ca>
2025-01-24 20:30:34 -0500

    Made a barn. Gave the clouds movement that resets the position to the left side of the screen

---
+++ Assets/Script/Mechanical Drawing/Clouds movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Cloudsmovement : MonoBehaviour
+{
+    public float Speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cloudPos = transform.position;
+       
+        cloudPos.x += Speed * Time.deltaTime;
+
+        Vector2 cloudsInScreenSpace = Camera.main.WorldToScreenPoint(cloudPos);
+        if( cloudsInScreenSpace.x > Screen.width)
+        {
+            // takes the position in game space then subtracks it by itself *2
+            // no matter the size of the screen the clouds will respawn in the right side of the screen
+            cloudPos.x = cloudPos.x - cloudPos.x*2;       
+        }
+        
+        transform.position = cloudPos;
+    }
+}
