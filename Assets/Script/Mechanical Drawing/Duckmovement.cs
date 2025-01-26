63f0a408ff33df03790fb0c1d0cd5176219e6091
LAPTOP-BITE348R\Anthony <pattisoa@sheridancollege.ca>
2025-01-25 21:36:18 -0500

    gave  the ducks collison with the 4 parts of the screen to keep them inside of the scene

--- Assets/Script/Mechanical Drawing/Duckmovement.cs
+++ Assets/Script/Mechanical Drawing/Duckmovement.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Duckmovement : MonoBehaviour
 {
-    public float speed = 5;
+    public Transform start;
+    public Transform end;
+
+   public AnimationCurve anim;
+    [Range (0f,1f)]
+    public float t;
+    public float speed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +21,24 @@ public class Duckmovement : MonoBehaviour
     void Update()
     {
         Vector2 pos = transform.position;
-        pos.x += Input.GetAxis("Horizontal") * speed *Time.deltaTime;
+        pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
-
+        if (pos.x < -15 ) {
+            pos.x = -14.99f;
+        }
+        if (pos.x > 5)
+        {
+            pos.x = 4.99f;
+        }
+        if (pos.y < -2.7)
+        {
+            pos.y = -2.69f;
+        }
+        if (pos.y > 2.5)
+        {
+            pos.y = 2.49f;
+        }
         transform.position = pos;
+        
     }
 }
